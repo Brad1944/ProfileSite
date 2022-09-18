f9fe3dc1f567c1cc1971cb68fd0ddcb2c15bae39
Afif87123 <apip@apip.pw>
2022-09-18 20:40:33 +0700

    Added Favicon

--- api/main.js
+++ api/main.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-res.set('view engine', 'ejs')
+router.set('view engine', 'ejs')
 
 router.get('/', async (req, res) => {
     res.render('index')
