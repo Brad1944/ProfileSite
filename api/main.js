bfd885504ddf3d1d857e3fe111e392eb1c9998ce
Afif87123 <apip@apip.pw>
2022-09-18 20:48:10 +0700

    whoa

--- api/main.js
+++ api/main.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-    res.render('index')
+    res.return('shit is working!?!')
   })
 
 module.exports = router;
--- index.js
+++ index.js
@@ -10,7 +10,9 @@ app.set('view engine', 'ejs')
 app.use("/api/main", main)
 app.use(express.static(__dirname + '/public'));
 
-
+app.get('/', (req, res) => {
+  res.render('index')
+})
 
 app.listen(port, () => {
   console.log(`Website running on port ${port} 🤙`)
