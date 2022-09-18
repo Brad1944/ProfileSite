100e40f62d43a30825428a628ae30872540df78b
Afif87123 <apip@apip.pw>
2022-09-18 20:16:55 +0700

    added apis

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const app = express()
 const main = require("./api/main")
 const port = process.env.PORT || 8080;
 
+
+app.use(express.json({ extended: false }));
 app.set('view engine', 'ejs')
 app.use("/api/main", main)
-app.use(express.static(__dirname + '/public'));
+//app.use(express.static(__dirname + '/public'));
 
 
 
