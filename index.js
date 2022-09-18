9beafaceb5a8c8b17edfd8d60e421fe74a4f3871
Afif87123 <apip@apip.pw>
2022-09-18 20:14:26 +0700

    added apis

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const app = express()
+const main = require("./api/main")
 const port = process.env.PORT || 8080;
 
 app.set('view engine', 'ejs')
+app.use("/api/main", main)
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', async (req, res) => {
-  res.render('index')
-})
+
 
 app.listen(port, () => {
   console.log(`Website running on port ${port} 🤙`)
